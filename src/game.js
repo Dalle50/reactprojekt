59a0b4b6010a74294122d084ce1114c9a36af4aa
danie <danie@DESKTOP-VPF1S1R>
2024-03-14 08:49:09 +0100

    modified:   src/game.js 	modified:   src/index.js 	modified:   src/login.js

--- src/game.js
+++ src/game.js
@@ -1,19 +1,46 @@
 import React, { useState } from "react";
 import useGameServer from "./useGameServer";
 
-function Game(token) {
-    const gameHubUrl = "http://react.tsanas.com/gamehub";
-    const onConnectionClosed = (error) => {
-      console.error("Connection closed:", error);
-    };
-    const [server] = useState(useGameServer(gameHubUrl, token.token, onConnectionClosed));
-    function connectToGame() {
-        server.connect();
-    }
+function Game({ token, onLogOut }) {
+  const gameHubUrl = "http://react.tsanas.com/gamehub";
+  const [gameIsConnected, setGameIsConnected] = useState(false);
+
+  const onConnectionClosed = (error) => {
+    console.error("Connection closed:", error);
+    setGameIsConnected(false);
+  };
+
+  const [server] = useState(useGameServer(gameHubUrl, token, onConnectionClosed));
+
+  const connectToGame = () => {
+    server.connect();
+    setGameIsConnected(true);
+  };
+
+  const disconnectFromGame = () => {
+    server.disconnect();
+    onLogOut(); // Call the onLogOut function passed as prop from App component
+    setGameIsConnected(false);
+  };
+
   return (
     <div>
       <h2>Game</h2>
-      <button onClick={connectToGame}>Connect to game</button>
+      {!gameIsConnected && (
+        <>
+          <button id="connectBtn" onClick={connectToGame}>
+            Connect to game
+          </button>
+        </>
+      )}
+      {gameIsConnected && (
+        <>
+          <div>Game is connected</div>
+          <button id="disconnectBtn" onClick={disconnectFromGame}>
+            Disconnect from game
+          </button>
+        </>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-import React, { useState, useEffect, useSyncExternalStore, useCallback } from "react"; // Importing useEffect
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Login from "./login";
-import useGameServer from "./useGameServer";
 import Game from "./game";
-
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [authToken, setToken] = useState(null);
-  const gameHubUrl = "http://react.tsanas.com/gamehub";
-  const onConnectionClosed = (error) => {
-    console.error("Connection closed:", error);
-  };
-  const [gameServer, setGameServer] = useState(null); // Declaring gameServer
+
   const handleLogin = (authToken) => {
+    if (authToken.success) {
+      console.log("Invalid credentials");
+      setIsLoggedIn(false);
+      return;
+    }
     setIsLoggedIn(true);
     setToken(authToken);
     console.log("Auth : " + authToken);
   };
-  
+
+  const disconnectFromGame = () => {
+    setIsLoggedIn(false);
+    setToken(null);
+  };
+
   return (
     <div>
       {!isLoggedIn && <Login onLogin={handleLogin} />}
-      {isLoggedIn && authToken  && <Game token={ authToken } />}
+      {isLoggedIn && authToken && (
+        <Game token={authToken} onLogOut={disconnectFromGame} />
+      )}
     </div>
   );
 }
